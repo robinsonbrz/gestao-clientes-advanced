3ca7d07d538f89d864f57b766986a79798a99cc9
Robinson Enedino <robinsonbrz@gmail.com>
2023-08-31 23:00:43 -0300

    feat: comentarios template filters

--- clientes/templatetags/filters.py
+++ clientes/templatetags/filters.py
@@ -4,4 +4,15 @@ register = template.Library()
 
 @register.filter
 def arredonda(value, casas):
+    '''
+    Template tag personalizado
+    Arredonda o valor para o número de casas informado
+    exemplo de uso :
+    numero a ser arredondado : 123.6548
+    numero de casas : 2
+    resultado : 123.65
+     
+    {{ 123.6548| arredonda:2}}
+    '''
     return round(value, casas)
+
