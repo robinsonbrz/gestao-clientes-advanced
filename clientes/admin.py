52c3aea7913a8add91ad70846bc404af20f51b34
Robinson Enedino <robinsonbrz@gmail.com>
2023-09-04 23:58:48 -0300

    feat: list_display

--- clientes/admin.py
+++ clientes/admin.py
@@ -3,6 +3,12 @@ from .models import Person, Documento
 
 
 class PersonAdmin(admin.ModelAdmin):
+    # fields= seriam campos do model que desejamos que seja exibido no admin do django
+    # podemos exibir todos e utilizar o 
+    # exclude = para excluir algum campo do model
+    # podemos utilizar o fieldsets para organizar os campos do model
+    
+
     fieldsets = (
         ('Dados pessoais', {'fields': ('first_name', 'last_name', 'doc', 'telefone')}),
         ('Dados complementares', {
@@ -13,6 +19,7 @@ class PersonAdmin(admin.ModelAdmin):
     # fields = (('doc', 'first_name'), 'last_name', ('age', 'salary'), 'bio', 'photo')
     # exclude = ('bio', )
     list_filter = ('age', 'salary')
+    # Set list_display to control which fields are displayed on the change list page of the admin
     list_display = ('doc', 'first_name', 'last_name', 'age', 'salary', 'bio', 'tem_foto')
     search_fields = ('id', 'first_name')
     autocomplete_fields = ['doc']
