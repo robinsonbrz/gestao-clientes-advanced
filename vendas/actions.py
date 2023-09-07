2cca4ed4cab2ec6b54e49524305c8afb79e32624
Robinson Enedino <robinsonbrz@gmail.com>
2023-09-07 00:05:09 -0300

    feat: actions

--- vendas/actions.py
+++ vendas/actions.py
@@ -1,12 +1,26 @@
 from django.http import HttpResponseNotFound
 
+'''
+https://docs.djangoproject.com/en/4.2/ref/contrib/admin/actions/
+Registrando actions no admin
+No exemplo abaixo registramos duas actions
+que setam um booleano como False ou True
+Nesse caso o field nfe_emitida
 
+'''
 def nfe_emitida(modeladmin, request, queryset):
+    '''
+    Define nota fiscal emitida True se usuário tiver permissão
+    '''
     if request.user.has_perm('vendas.setar_nfe'):
         queryset.update(nfe_emitida=True)
     else:
         return HttpResponseNotFound('<h1>Sem permissao</h1>')
 
+
+'''
+Descrição da action
+'''
 nfe_emitida.short_description = "NF-e emitida"
 
 
