9580cf37a260f1ec11d949e59790109ab6d6674f
Robinson Enedino <robinsonbrz@gmail.com>
2023-09-20 23:33:45 -0300

    feat: simple form

--- vendas/forms.py
+++ vendas/forms.py
@@ -5,3 +5,38 @@ class ItemPedidoForm(forms.Form):
     produto_id = forms.CharField(label='ID do Produto', max_length=100)
     quantidade = forms.IntegerField(label='Quantidade')
     desconto = forms.DecimalField(label='Desconto', max_digits=7, decimal_places=2)
+
+
+
+class MyForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    gender = forms.ChoiceField(choices=[('M', 'Masculino'), ('F', 'Feminino')])
+
+'''
+# Exemplo de uso:
+
+form = MyForm()
+
+# Preenche o form com dados do usuário:
+
+form.cleaned_data['name'] = 'João da Silva'
+form.cleaned_data['gender'] = 'M'
+
+# Valida o form:
+
+if form.is_valid():
+    print(form.cleaned_data)
+Use code with caution. Learn more
+Este código irá gerar o seguinte formulário HTML:
+
+HTML
+<form action="/" method="post">
+    <input type="text" name="name" placeholder="Nome">
+    <select name="gender">
+        <option value="M">Masculino</option>
+        <option value="F">Feminino</option>
+    </select>
+    <input type="submit" value="Enviar">
+</form>
+
+'''
